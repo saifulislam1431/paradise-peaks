e85fa0ae8d6b8aceabe321defbe3d3a0eca79795
Saiful Islam <sakibbhaiya143@gmail.com>
2023-04-24 20:24:15 +0600

    Firebase authentication provider

---
+++ src/AuthProviders/AuthProvider.jsx
@@ -0,0 +1,34 @@
+import React, { createContext, useEffect, useState } from 'react';
+import app from '../firebase/firebase.config';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+
+export const UserContext = createContext(null);
+
+
+const auth = getAuth(app);
+const AuthProvider = ({ children }) => {
+    const [user , setUser] =useState(null);
+
+    
+
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth , currentUser=>{
+            setUser(currentUser)
+        })
+        return ()=>{
+            unsubscribe();
+        }
+    },[])
+    const userInfo = {
+        user
+    }
+    return (
+        <UserContext.Provider value={userInfo}>
+            {children}
+        </UserContext.Provider>
+    );
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import Router from './Routes/Router.jsx'
+import AuthProvider from './AuthProviders/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthProvider>
     <RouterProvider router={Router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
