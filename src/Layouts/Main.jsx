551b97e9c7342c2cb83a5f01c11fb81dd82572cd
Saiful Islam <sakibbhaiya143@gmail.com>
2023-04-26 01:01:30 +0600

    Path location navigator added

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../Pages/Shared/Header/Header';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Footer from '../Pages/Shared/Footer/Footer';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Main = () => {
+    const loc = useLocation();
+    useEffect(()=>{
+        if(loc.pathname == "/"){
+            document.title("Paradise peaks")
+        }else{
+            document.title=`Paradise peaks ${loc.pathname.replace("/", "| ")}`
+        }
+    },[loc.pathname])
     return (
         <main className='myContainer'>
             <Header></Header>
